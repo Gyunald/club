7c88d912c2767d440c855668144cf808a39ab8b8
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-13 16:49:52 +0900

    Update club.py

--- pages/club.py
+++ pages/club.py
@@ -45,13 +45,14 @@ if nickname :
         club = st.selectbox('club',[st.session_state.club])
         date = st.date_input('날짜',(datetime.datetime.utcnow()+datetime.timedelta(hours=9))).strftime('%Y-%m-%d')
         time = st.time_input('시간',value= datetime.time(17,45)).strftime('%H:%M')
-
+        
         empty = st.empty()
-        place = empty.selectbox('장소',st.session_state.place)
+        place = empty.selectbox('장소',st.session_state.place,help='장소를 직접 입력하려면 장소추가 버튼을 누르세요.')
+        people = st.number_input('정원',value=10,max_value=30,help='최대인원 30명')
         button_place = st.form_submit_button('장소추가',use_container_width=True)
         button_place_del = st.form_submit_button('장소삭제',use_container_width=True)
         if button_place:
-            place = empty.text_input('place',placeholder='장소 직접입력 후 리스트에 추가')
+            place = empty.text_input('place',placeholder='장소 직접입력 후 리스트에 추가',max_chars=30,help='장소추가 버튼을 한번 더 누르세요.')
             if place != '' and place not in st.session_state.place:
                 st.session_state.place.append(place)
                 place = empty.selectbox('place',st.session_state.place,key='place_append')
@@ -67,9 +68,10 @@ if nickname :
             '날짜' : date,
             '장소' : place,
             '참가목록' : [],
+            '인원수' : 0,
             '불참가목록' : [],
             '참여' : {},
-            '불참' : {},
+            '불참' : {},            
             '작성자' : nickname,
         }}
         doc_ref = st.session_state.doc_ref.document(club)
@@ -99,11 +101,12 @@ if nickname :
         doc_list = doc_document.get('참가목록')
         doc_list_non = doc_document.get('불참가목록')
         standard = (datetime.datetime.utcnow()+datetime.timedelta(hours=9)).strftime('%Y-%m-%d') > doc_document['날짜']
+        
         k = f"disabled_{j}"
         if k not in st.session_state:
             st.session_state[k] = False
 
-        if len(doc_list) == 2:
+        if len(doc_list) == people:
             st.session_state[k] = True
             if nickname in doc_list:
                 st.session_state[k] = False
@@ -140,10 +143,11 @@ if nickname :
                             doc_ref.update({f"{doc_document.get('날짜')}-{doc_document.get('장소')}" : firestore.DELETE_FIELD})
                             st.experimental_rerun()
                 if 참 :
-                    if len(doc_list) < 2:
+                    if len(doc_list) < people:
                         doc_application = doc_document.get('참여')
                         if nickname not in doc_list:
                             doc_list.append(nickname)
+                            doc_document['인원수'] +=1
                             doc_application[nickname] = doc_time
                             if nickname in doc_list_non:
                                 doc_list_non.remove(nickname)
@@ -153,22 +157,23 @@ if nickname :
                 if 불참:
                     doc_cancel = doc_document.get('불참')
                     if nickname not in doc_list_non :
-                        doc_list_non.append(nickname)
+                        doc_list_non.append(nickname)                        
                         doc_cancel[nickname] = doc_time
                         if nickname in doc_list:
                             doc_list.remove(nickname)
+                            doc_document['인원수'] -=1
                     doc_ref.update(doc)
                     st.experimental_rerun()
 
-                if len(doc_list) == 2:
-                    st.error(f"{len(doc_list)}/2 명")
+                if len(doc_list) == people:
+                    st.error(f"{len(doc_document['인원수'])}/{people} 명")
                     if nickname in doc_list:
                         st.session_state.disabled_불참 = False
                     else:
                         st.session_state.disabled_참 = True
                         st.session_state.disabled_불참 = True
                 else:
-                    st.info(f"{len(doc_list)}/2 명")
+                    st.info(f"{len(doc_list)}/{people} 명")
                 st.error(doc_list)
                 st.info(doc_list_non)
                 word = doc_document.get('장소').replace(' ','')

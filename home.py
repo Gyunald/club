18ec3c92e4a4f0a322685ef2d751025188a5f704
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-14 15:40:39 +0900

    Update home.py

--- home.py
+++ home.py
@@ -62,15 +62,15 @@ if nickname:
             if st.button('배드민턴',use_container_width=True) :
                 st.session_state.club = '배드민턴'
                 st.session_state.place = ['고양 팩토 스타디움','파주시 배드민턴 전용구장']
-                st.session_state.doc_ref = db.collection('activity')
+                st.session_state.doc_ref = db.collection('dynamic')
                 switch_page('club')
             
         with c[1]:
             img('https://cdn.pixabay.com/photo/2019/01/21/13/58/table-tenis-3946115_960_720.jpg')
             if st.button('탁구',use_container_width=True):
-                st.session_state.club = '탁구'
+                st.session_state.club = ' 탁구'
                 st.session_state.place = ['문산 국민체육센터']
-                st.session_state.doc_ref = db.collection('activity')
+                st.session_state.doc_ref = db.collection('dynamic')
                 switch_page('club')
 
         with c[2]:
@@ -78,17 +78,41 @@ if nickname:
             if st.button('축구',use_container_width=True):
                 st.session_state.club = '축구'
                 st.session_state.place = ['당동리 풋살구장','FC 파주 풋살장']
-                st.session_state.doc_ref = db.collection('activity')
+                st.session_state.doc_ref = db.collection('dynamic')
                 switch_page('club')
 
     with expander('static'):
         c = st.columns(3)
-
         with c[0]:
-            img('https://cdn.pixabay.com/photo/2015/11/20/08/17/meat-1052571_960_720.jpg')
-            if st.button('파티',use_container_width=True):
-                st.session_state.club = '파티'
-                st.session_state.place = ['삼겹살','닭발','치킨']
+            img('https://cdn.pixabay.com/photo/2017/12/17/21/44/drink-3025022__340.jpg')
+            if st.button('독서',use_container_width=True):
+                st.session_state.club = '독서'
+                st.session_state.place = ['폴바셋', '스타벅스']
+                st.session_state.doc_ref = db.collection('static')
+                switch_page('club')
+
+        with c[1]:
+            img('https://cdn.pixabay.com/photo/2015/05/26/23/52/technology-785742_960_720.jpg')
+            if st.button('코딩',use_container_width=True):
+                st.session_state.club = '코딩'
+                st.session_state.place = ['스타벅스','폴바셋']
+                st.session_state.doc_ref = db.collection('static')
+                switch_page('club')
+
+        with c[2]:
+            img('https://cdn.pixabay.com/photo/2021/10/31/07/23/instrument-6756414__340.jpg')
+            if st.button('기타',use_container_width=True):
+                st.session_state.club = '기타'
+                st.session_state.place = ['스타벅스','폴바셋']
+                st.session_state.doc_ref = db.collection('static')
+                switch_page('club')
+                
+    with expander('etc'):
+        c = st.columns(3)
+        with c[0]:
+            img('https://cdn.pixabay.com/photo/2017/06/21/09/19/spoon-2426623__340.jpg')
+            if st.button('모임',use_container_width=True):
+                st.session_state.club = '모임'                
                 st.session_state.doc_ref = db.collection('static')
                 switch_page('club')
 

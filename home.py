a38b4dd4b71861d9abc63b4dbc4713a415898750
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-17 11:49:03 +0900

    Update home.py

--- home.py
+++ home.py
@@ -70,12 +70,11 @@ db = firestore.client()
 if nickname:
     empty.empty()
     st.write('# IMI CE Korea Club')
-    st.write('---')
     with server_state_lock["chat_messages"]:
         if "chat_messages" not in server_state:
             server_state["chat_messages"] = []
             
-    st.text_area('Chat','\n'.join(server_state["chat_messages"]),height=150)
+    st.text_area('Chat','\n'.join(server_state["chat_messages"]),height=100)
 
     st.text_input("Message", key="message_input", on_change=on_message_input)
     

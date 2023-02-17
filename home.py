347a1aa9875c4adedb9a7fa623091d172c4a8649
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-17 20:57:20 +0900

    Update home.py

--- home.py
+++ home.py
@@ -187,7 +187,7 @@ def on_message_input():
 #         ]
 
     with server_state_lock["chat_messages"]:
-        server_state["chat_messages"] = server_state["chat_messages"].insert(0,new_message_packet)
+        server_state["chat_messages"].append(new_message_packet)
 
 with server_state_lock["chat_messages"]:
     if "chat_messages" not in server_state:

d98699a32dece18658f494a208040ca4c318e1b7
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-17 21:52:30 +0900

    Update home.py

--- home.py
+++ home.py
@@ -203,14 +203,15 @@ with server_state_lock["chat_messages"]:
         server_state["text"] = []
 
 e.empty()
-for i in server_state["chat_messages"][-1]:
-    server_state["text"].insert(0,f"{i['nickname']} : {i['text']}\n{times()}")
-
-a= st.text_input("Message", key="message_input", on_change=on_message_input)
-st.text_area('Chat','\n'.join(server_state["text"]), height=150)
 
 if st.button('clear'): 
     server_state["chat_messages"] = []
     server_state["text"] = []
     st.experimental_rerun()
+
+for i in server_state["chat_messages"][-1]:
+    server_state["text"].insert(0,f"{i['nickname']} : {i['text']}\n{times()}")
+
+a= st.text_input("Message", key="message_input", on_change=on_message_input)
+st.text_area('Chat','\n'.join(server_state["text"]), height=150)
 

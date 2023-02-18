d1a4f92aabe361dd7e7677601c883671587e18a0
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-18 16:48:18 +0900

    Update home.py

--- home.py
+++ home.py
@@ -185,7 +185,7 @@ def on_message_input():
         server_state["chat_messages"] = server_state["chat_messages"] + [
             f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"
         ]
-
+    return new_message_packet['nickname']
 with server_state_lock["chat_messages"]:
     if "chat_messages" not in server_state:
         server_state["chat_messages"] = []
@@ -196,8 +196,7 @@ if st.button('clear'):
     server_state["chat_messages"] = []
     server_state["text"] = []
     st.experimental_rerun()
-    
-new_message_packet['nickname']
-# st.info('\n'.join(new_message_packet['nickname']))
+
+st.info('\n'.join(on_message_input()))
 st.text_input("Message", key="message_input", on_change=on_message_input)
 st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)

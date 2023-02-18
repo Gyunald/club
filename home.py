7ae35830b5c3807bf4b00b0177053edec5465d28
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-18 16:56:22 +0900

    Update home.py

--- home.py
+++ home.py
@@ -198,6 +198,6 @@ if st.button('clear'):
     server_state["text"] = []
     st.experimental_rerun()
 
-st.info('\n'.join(server_state["chat_messages"]).split()[0])
+st.info('\n'.join(server_state["chat_messages"]).split())
 st.text_input("Message", key="message_input", on_change=on_message_input)
 st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)

481b9fe3e9a485fce6e1430bb2f4983591ddcd64
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-19 15:28:18 +0900

    Update home.py

--- home.py
+++ home.py
@@ -186,7 +186,7 @@ def on_message_input():
     
     with server_state_lock["chat_messages"]:
         if (datetime.utcnow()+timedelta(hours=9)).minute != server_state["chat_messages"][-1].split(' ')[4].split(':')[1]:
-        server_state["chat_messages"] = server_state["chat_messages"] + [
+            server_state["chat_messages"] = server_state["chat_messages"] + [
             f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"
         ]
         else:

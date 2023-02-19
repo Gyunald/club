89e75e7a60976f95406ed3f7bd96dbffb1cd6f10
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-19 22:23:18 +0900

    Update home.py

--- home.py
+++ home.py
@@ -156,58 +156,90 @@ st.set_page_config(
 #         st.session_state.clear()
 #         switch_page('club')
 
-import streamlit as st
-from datetime import datetime,timedelta
-from streamlit_server_state import server_state, server_state_lock
+# import streamlit as st
+# from datetime import datetime,timedelta
+# from streamlit_server_state import server_state, server_state_lock
  
-e =st.empty()
-nickname = e.text_input("Nick name", key="nickname")
+# e =st.empty()
+# nickname = e.text_input("Nick name", key="nickname")
 
-if not nickname:    
-    st.stop()
+# if not nickname:    
+#     st.stop()
 
-def on_message_input():
-    new_message_text = st.session_state["message_input"]
-    if not new_message_text:
-        return 
+# def on_message_input():
+#     new_message_text = st.session_state["message_input"]
+#     if not new_message_text:
+#         return 
 
 #     st.session_state["chat_messages"] = st.session_state["message_input"]
 #     st.session_state["message_input"] = ""
 
+#     new_message_packet = {
+#         "nickname": nickname,
+#         "text": new_message_text,
+#         "time": (datetime.utcnow()+timedelta(hours=9)).strftime('%H:%M:%S')
+#     }
+
+#     with server_state_lock["chat_messages"]:
+#         server_state["chat_messages"] = server_state["chat_messages"] + [
+#             f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"
+#         ]
+        
+        
+# st.write(f"### HELLO, {nickname}🎈")
+# with server_state_lock["chat_messages"]:
+#     if "chat_messages" not in server_state:
+#         server_state["chat_messages"] = []
+#     if "user" not in server_state:        
+#         server_state["user"] = [nickname]
+        
+#     else:
+#         if nickname not in server_state["user"]:
+#             server_state["user"].append(nickname)
+
+    
+# e.empty()
+
+# if st.button('clear'): 
+#     server_state["chat_messages"] = []
+#     st.experimental_rerun()
+    
+# # if st.button('user_clear'): 
+# #    server_state["user"] = [nickname]
+# #    st.experimental_rerun()
+    
+# st.info('\n'.join(server_state["user"]))
+# st.text_input("Message", key="message_input", on_change=on_message_input)
+# st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)
+
+import streamlit as st
+
+from streamlit_server_state import server_state, server_state_lock
+
+nickname = st.text_input("Nick name", key="nickname")
+if not nickname:
+    st.stop()
+
+
+def on_message_input():
+    new_message_text = st.session_state["message_input"]
+    if not new_message_text:
+        return
+
     new_message_packet = {
         "nickname": nickname,
         "text": new_message_text,
-        "time": (datetime.utcnow()+timedelta(hours=9)).strftime('%H:%M:%S')
     }
-
     with server_state_lock["chat_messages"]:
         server_state["chat_messages"] = server_state["chat_messages"] + [
-            f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"
+            new_message_packet
         ]
-        
-        
-st.write(f"### HELLO, {nickname}🎈")
+
+
 with server_state_lock["chat_messages"]:
     if "chat_messages" not in server_state:
         server_state["chat_messages"] = []
-    if "user" not in server_state:        
-        server_state["user"] = [nickname]
-        
-    else:
-        if nickname not in server_state["user"]:
-            server_state["user"].append(nickname)
 
-    
-e.empty()
-
-if st.button('clear'): 
-    server_state["chat_messages"] = []
-    st.experimental_rerun()
-    
-# if st.button('user_clear'): 
-#    server_state["user"] = [nickname]
-#    st.experimental_rerun()
-    
-st.info('\n'.join(server_state["user"]))
 st.text_input("Message", key="message_input", on_change=on_message_input)
-st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)
+
+st.write(server_state["chat_messages"])

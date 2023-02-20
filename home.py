96c979f561169006807e1f44b46ece04759fd938
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-20 22:42:24 +0900

    Update home.py

--- home.py
+++ home.py
@@ -232,7 +232,6 @@ if nickname:
 
     st.info('\n'.join(set(server_state["user"])))
     st.text_input("Message", key="message_input", on_change=on_message_input)
-    st.text_input("Message", key="text", on_change=on_message_input)
     st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)
 
     st.write(server_state.chat_messages)

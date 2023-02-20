47d5f4a825a4deff58b31de655c66bc5a54a70fa
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-20 22:33:59 +0900

    Update home.py

--- home.py
+++ home.py
@@ -185,10 +185,8 @@ from streamlit_server_state import server_state, server_state_lock, no_rerun
 
 def on_message_input():
     new_message_text = st.session_state["message_input"]
-    text = st.session_state["text"]
 
-    st.session_state["text"] = st.session_state["message_input"]
-    st.session_state["message_input"] = ""
+    st.session_state["text"] = new_message_text
 
     server_state["user"] = [nickname]
 
@@ -204,14 +202,16 @@ def on_message_input():
             server_state["chat_messages"] = server_state["chat_messages"] + [
                 f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"
             ]
-
+    st.session_state["message_input"] = ""
+    
 e = st.empty()
 nickname = e.text_input('Nickname')
 
 if nickname:
     e.empty()
     st.write(f"### Hi, {nickname}🎈")
-
+    if 'text' not in st.session_state:
+        st.session_state["text"] = ''
     with server_state_lock["chat_messages"]:    
         if "chat_messages" not in server_state:
             server_state["chat_messages"] = []
@@ -222,7 +222,7 @@ if nickname:
     else:
         if nickname not in server_state["user"]:
             server_state["user"] = [nickname] + server_state["user"]
-
+    
     if st.button('claer'): 
         server_state["chat_messages"] = []
 
@@ -231,7 +231,7 @@ if nickname:
         server_state.clear()
 
     st.info('\n'.join(set(server_state["user"])))
-    st.text_input("Message", key="text", on_change=on_message_input)
+    st.text_input("Message", key="message_input", on_change=on_message_input)
     st.text_area('Chat','\n'.join(server_state["chat_messages"][::-1]), height=150)
 
     st.write(server_state.chat_messages)

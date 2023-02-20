7c87884454bdadac40fa92409aee020627f225ce
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-02-20 23:00:58 +0900

    Update home.py

--- home.py
+++ home.py
@@ -187,7 +187,7 @@ def on_message_input():
     new_message_text = st.session_state["message_input"]
 
     st.session_state["message_input"] = ""
-    server_state["user"] = [nickname]
+#     server_state["user"] = [nickname]
 
     if not new_message_text:
         return
@@ -197,8 +197,7 @@ def on_message_input():
         "text": new_message_text,
         "time": (datetime.utcnow()+timedelta(hours=9)).strftime('%H:%M:%S')
     }
-    with no_rerun():
-        st.session_state["message_input"] = "1"
+    
     with server_state_lock["chat_messages"]:
             server_state["chat_messages"] = server_state["chat_messages"] + [
                 f"{new_message_packet['nickname']} : {new_message_packet['text']} \n {new_message_packet['time']}"

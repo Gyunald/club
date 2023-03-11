26a5d45b0bdd9c42944d036c13ec7059388caf63
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-03-12 00:18:53 +0900

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -16,7 +16,7 @@ def on_message_input():
     new_message_packet = {
         "nickname": nickname,
         "text": new_message_text,
-        "time": (datetime.utcnow()+timedelta(hours=9)).strftime('%m.%d %H:%M:%S'))
+        "time": (datetime.utcnow()+timedelta(hours=9)).strftime('%m.%d %H:%M:%S')
     }
 
     with server_state_lock["chat_messages"]:

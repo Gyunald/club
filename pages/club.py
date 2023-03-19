18318cdf96aab76f846423d24c94ab85be0ba7a8
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-03-19 18:13:41 +0900

    Update club.py

--- pages/club.py
+++ pages/club.py
@@ -481,7 +481,7 @@ st.markdown("""
     }
 
     .stTextArea [data-baseweb=base-input] [disabled=""]{
-        -webkit-text-fill-color: rgb(80, 80, 80);
+        -webkit-text-fill-color: rgb(60, 60, 60);
     }
     </style>
     """,unsafe_allow_html=True)
